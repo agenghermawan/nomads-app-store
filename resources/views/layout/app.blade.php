8c516fbb59cdd06337391ae0920f0a71ae280d21
Hermawan <agenghermawan16@gmail.com>
2020-12-12 09:09:56 +0700

    nomads

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{ url('frontend/backend/libraries/bootstrap/css/bootstrap.css') }}">
+    <link  
+        href="https://fonts.googleapis.com/css?family=Assistant:200,300,400,600,700,800|
+        Playfair+Display:400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap" 
+        rel="stylesheet">
+    <link rel="stylesheet" href="{{ url('frontend/backend/styles/main.css') }}">
+</head>
+<body>
+
+    <!-- navbar -->
+
+    <div class="container">
+        <nav class="row navbar navbar-expand-lg navbar-light bg-white">
+            <a href="#" class="navbar-brand">
+                <img src="{{ url('frontend/backend/images/logo.png') }}" alt="logo NOMADS">
+            </a>
+            <button 
+                class="navbar-toggler navbar-toggler-right"
+                type="button"
+                data-toggle="collapse"
+                data-target="#navb"
+            >
+            <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navb">
+                <ul class="navbar-nav ml-auto mr-3">
+                    <li class="nav-item mx-md-2">
+                        <a href="#" class="nav-link active">Home</a>
+                    </li>
+
+                    <li class="nav-item mx-md-2">
+                        <a href="#" class="nav-link">Paket Travel</a>
+                    </li>
+
+                    <li class="nav-item dropdown">
+                        <a href="#" class="nav-link dropdown-toggle" id="navbardrop" data-toggle="dropdown">
+                            Service
+                        </a>
+                        <div class="dropdown-menu">
+                            <a href="#" class="dropdown-item">link</a>
+                            <a href="#" class="dropdown-item">link</a>
+                            <a href="#" class="dropdown-item">link</a>
+                        </div>
+                    </li>
+
+                    <li class="nav-item mx-md-2">
+                        <a href="#" class="nav-link">Testimonial</a>
+                    </li>
+                </ul>
+
+                <!-- Mobile Button -->
+                <form class="form-inline d-sm-block d-md-none">
+                    <button class="btn btn-login my-2 my-sm-0">
+                        Masuk
+                    </button>
+                </form>
+
+                <!-- Dekstop Button  -->
+                <form class="form-inline my-2 my-lg-0 d-none d-md-block">
+                    <button class="btn btn-login btn-navbar-right my-2 my-sm-0 px-4">
+                        Masuk
+                    </button>
+                </form>
+
+            </div>
+        </nav>
+    </div>    
+
+
+    @yield('content')
+
+    <!-- main -->
+
+    <footer class="section-footer mt-5 mb-4 border-top">
+        <div class="container pt-5 pb-5">
+            <div class="row justify-content-center">
+                <div class="col-12">
+                    <div class="row">
+                        <div class="col-12 col-lg-3">
+                            <h5>FEATURES</h5>
+                            <ul class="list-unstyled">
+                                <li><a href="#">Reviews</a></li>
+                                <li><a href="#">Community</a></li>
+                                <li><a href="#">Social Media Kit</a></li>
+                                <li><a href="#">Affiliate</a></li>
+                            </ul>
+                        </div>
+                        <div class="col-12 col-lg-3">
+                            <h5>ACCOUNT</h5>
+                            <ul class="list-unstyled">
+                                <li><a href="#">Refund</a></li>
+                                <li><a href="#">Security</a></li>
+                                <li><a href="#">Rewards</a></li>
+                            </ul>
+                        </div>
+                        <div class="col-12 col-lg-3">
+                            <h5>COMPANY</h5>
+                            <ul class="list-unstyled">
+                                <li><a href="#">Career</a></li>
+                                <li><a href="#">Help Center</a></li>
+                                <li><a href="#">Media</a></li>
+                            </ul>
+                        </div>
+                        <div class="col-12 col-lg-3">
+                            <h5>GET CONNECTED</h5>
+                            <ul class="list-unstyled">
+                                <li><a href="#">Jakarta Pusat</a></li>
+                                <li><a href="#">Indonesia</a></li>
+                                <li><a href="#">0821 - 222 - 1123</a></li>
+                                <li><a href="#">support@nomads.com</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="container-fluid">
+            <div class="row border-top justify-content-center align-items-center pt-4">
+                <div class="col-auto text-gray-500 font-weight-light">
+                    2019 Copyright Nomads • All rights reserved • Made in Jakarta
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <script src="{{ url('frontend/backend/libraries/jquery/jquery-3.4.1.min.js') }}"></script>
+    <script src="{{ url('frontend/backend/libraries/bootstrap/js/bootstrap.js') }}"></script>
+    <script src="{{ url('frontend/backend/libraries/retina/retina.min.js') }}"></script>
+</body>
+</html>
